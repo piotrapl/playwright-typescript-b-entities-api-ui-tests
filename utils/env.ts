95eeacefb86399c9ded0ec27316d566097881ee1
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-23 13:20:45 +0100

    dodanie def. środowiska nr 2 + selectedEnv (środowisko wskazane jako bieżące)

--- utils/env.ts
+++ utils/env.ts
@@ -4,10 +4,21 @@
 // Fraza "export const ENVIRONMENTS":
 //   eksport stałej o nazwie ENVIRONMENTS, 
 //   która jest obiektem zawierającym konfiguracje środowiskowe.
+//
+// prod i staging to dwa środowiska, które tutaj mają tę samą wartość baseURL,
+// ale potencjalnie mogą być różne w zależności od potrzeb.
 
 export const ENVIRONMENTS = {
 
     prod: {
         baseURL: 'https://wyszukiwarkaregon.stat.gov.pl/appBIR/index.aspx'
+    },
+
+    staging: {
+        baseURL: 'https://wyszukiwarkaregon.stat.gov.pl/appBIR/index.aspx'
     }
-}
+};
+
+const selectedEnv = process.env.TEST_ENV || 'prod';
+
+export const ENV = ENVIRONMENTS[selectedEnv];
