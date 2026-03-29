a69d61fe6b6ab5fdd21a2b38a7e16ed85249166f
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-29 19:44:04 +0200

    eksport testu i hook afterEach

--- fixtures/test-fixtures.ts
+++ fixtures/test-fixtures.ts
@@ -7,3 +7,44 @@ import { allure } from 'allure-playwright'
 import { RegonPage } from '../pages/regon.page';
 import { RegonFlow } from '../flows/regon.flow';
 
+type Fixtures = {
+    regonFlow: RegonFlow;
+}
+
+// Tu - eksportujemy naszą rozszerzoną wersję testu, 
+// która zawiera nasze własne fixture'y. Dzięki temu możemy korzystać z nich w naszych testach, 
+// importując ten plik i używając `test` zamiast `base`.
+export const test = base.extend<Fixture>({
+    regonFlow: async ({ page }, use) => {
+
+        const regonPage = new RegonPage(page);
+        const regonFlow = new RegonFlow(regonPage);
+
+        const   url = 'https://regon.stat.gov.pl/RegonRegonSearch.aspx';
+        await page.goto(url);
+        await use(regonFlow);
+
+    }
+});
+
+
+// hook `afterEach`, który będzie wykonywany po każdym teście.
+// arrow function ma 2 argumenty: `regonFlow` (nasze fixture) 
+// i `testInfo` (obiekt zawierający informacje o włąśnie zakończonym teście, który właśnie się zakończył).
+test.afterEach(async ({ regonFlow }, testInfo) => {
+
+    const screenshot = await page.screenshot();
+
+    await testInfo.attach('screenshot', {
+        body: screenshot,
+        contentType: 'image/png'
+    });
+
+    await allure.attachment(
+        'screenshot',
+        screenshot,
+        'image/png'
+    );
+
+});
+    
