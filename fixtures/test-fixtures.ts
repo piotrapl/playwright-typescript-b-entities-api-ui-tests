13d3bba0f98347816c42046b83599b76fe304b9f
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-03-29 20:03:57 +0200

    poprawka - inna ma być nawa właściwości obiektu przyjmowanego przez arrrow function (f. asynchr.)

--- fixtures/test-fixtures.ts
+++ fixtures/test-fixtures.ts
@@ -12,7 +12,7 @@ type Fixtures = {
 }
 
 // Tu - eksportujemy naszą rozszerzoną wersję testu, 
-// która zawiera nasze własne fixture'y. Dzięki temu możemy korzystać z nich w naszych testach, 
+// która zawiera nasze własne fixture'y. Wtedy możemy korzystać z nich w naszych testach, 
 // importując ten plik i używając `test` zamiast `base`.
 
 // base.extend<Fixtures> -  metoda, która pozwalalająca rozszerzyć bazowy test o nasze fixture'y.
@@ -34,8 +34,12 @@ export const test = base.extend<Fixtures>({
 
 // hook `afterEach`, który będzie wykonywany po każdym teście.
 // arrow function ma 2 argumenty: `regonFlow` (nasze fixture) 
-// i `testInfo` (obiekt zawierający informacje o włąśnie zakończonym teście, który właśnie się zakończył).
-test.afterEach(async ({ regonFlow }, testInfo) => {
+// i `testInfo` (obiekt zawierający informacje o właśnie zakończonym teście, który właśnie się zakończył).
+
+// async ({ page }, testInfo) => { - to funkcja asynchroniczna (arrow function), \
+// wołana po każdym teście, przyjmująca obiekt z właściwością `page` 
+// (reprezentującą stronę przeglądarki) i `testInfo` (zawierający informacje o teście).
+test.afterEach(async ({ page }, testInfo) => 
 
     const screenshot = await page.screenshot();
 
