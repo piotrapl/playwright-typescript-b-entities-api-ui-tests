2552ad7f0de781714406bc3a0051ce02d740476e
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-08 10:45:50 +0200

    dodanie brakującego zn. ":"

--- data/nonexisting-nips.ts
+++ data/nonexisting-nips.ts
@@ -6,9 +6,9 @@ export const nonExistingNips = [
         nip: '0755056162',
         description: 'NIP poprawny ale bez powiązań z istniejacymi podmiotami'
     },
-    
+
     {
         nip: '2818814189',
-        description 'NIP poprawny ale bez powiązań z istniejacymi podmiotami'
+        description: 'NIP poprawny ale bez powiązań z istniejacymi podmiotami'
     }  
 ]
