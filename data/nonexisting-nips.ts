b3ee5258433a3d23173c9e4a5f01d334491306ae
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-08 10:44:27 +0200

    dodanie kolejnego NIP-u

--- data/nonexisting-nips.ts
+++ data/nonexisting-nips.ts
@@ -5,5 +5,10 @@ export const nonExistingNips = [
     {
         nip: '0755056162',
         description: 'NIP poprawny ale bez powiązań z istniejacymi podmiotami'
-    }
+    },
+    
+    {
+        nip: '2818814189',
+        description 'NIP poprawny ale bez powiązań z istniejacymi podmiotami'
+    }  
 ]
