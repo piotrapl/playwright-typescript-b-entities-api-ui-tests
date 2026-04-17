824ee1f0c51b5fff4d046b1def72b23c78a1cce4
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-17 16:08:39 +0200

    dodanie moduł z testami dla NIP

---
+++ tests/nip.spec.ts
@@ -0,0 +1,14 @@
+import { test } from '../fixtures/test-fixtures';
+import { invalidNips } from '../data/invalid-nips';
+
+for (const dataset of invalidNips) {
+
+    test('Próba wyszukiwania po nieistniejącym NIP-ie: ${dataset.nip}', async ({ nipFlow }) => {
+
+        const result = await nipFlow.searchInvalidNip(dataset.nip);
+
+        result.assert();
+    
+    });
+
+}
