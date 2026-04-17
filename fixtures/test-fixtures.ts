c855a9c39acee7652ae832d38504437d9b152425
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-17 16:13:14 +0200

    dodanie importów dotyczących scenariusza dla NIP

--- fixtures/test-fixtures.ts
+++ fixtures/test-fixtures.ts
@@ -1,11 +1,14 @@
 // polecenia importujące różne moduły i klasy, które będziemy wykorzystywać w testach. 
 // Tu - importujemy test z biblioteki Playwright, allure z biblioteki allure-playwright oraz nasze własne klasy RegonPage i RegonFlow, które znajdują się w katalogu pages i flows.
 
-import {test as base} from '@playwright/test';
+import { test as base } from '@playwright/test';
 import { allure } from 'allure-playwright'
 
 import { RegonPage } from '../pages/regon.page';
 import { RegonFlow } from '../flows/regon.flow';
+
+import { NipPage } from '../pages/nip.page';
+import { NipFlow } from '../flows/nip.flow';
 
 type Fixtures = {
     regonFlow: RegonFlow;
