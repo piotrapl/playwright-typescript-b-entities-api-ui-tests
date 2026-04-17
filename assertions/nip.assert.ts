af31cc9de069dddb5f2f127f73544e4b7387cfbb
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-17 11:51:47 +0200

    dostosowanie klasy do rozwiązania z regon.assert.ts

--- assertions/nip.assert.ts
+++ assertions/nip.assert.ts
@@ -1,6 +1,8 @@
 import { expect } from '@playwright/test';
 import { ApiResponse } from '../types/api.types';
 
+// NipAssertions - to jest definicja klasy NipAssertions, 
+//                 która będzie zawierać metody do asercji dotyczących NIP-ów.
 export class NipAssertions {
 
     constructor {
@@ -10,4 +12,6 @@ export class NipAssertions {
 
     assert() {
         
+        expect (this.apiResponse.status).toBe(200);
+
         expect(this.uiMessage).toMatch(/Nie znaleziono/); 
