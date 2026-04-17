95b24a607d33c3aee7966705eb6cd39fc50fefb0
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-17 16:18:02 +0200

    dodanie słowa kluczowego 'as' do importu

--- tests/nip.spec.ts
+++ tests/nip.spec.ts
@@ -1,9 +1,14 @@
-import { test } from '../fixtures/test-fixtures';
+import { test as base } from '@playwright/test';
 import { invalidNips } from '../data/invalid-nips';
+import { nipFlow } from '../fixtures/test-fixtures';
+
+const test = base.extend({
+    nipFlow: nipFlow,
+});
 
 for (const dataset of invalidNips) {
 
-    test('Próba wyszukiwania po nieistniejącym NIP-ie: ${dataset.nip}', async ({ nipFlow }) => {
+    test(`Próba wyszukiwania po nieistniejącym NIP-ie: ${dataset.nip}`, async ({ nipFlow }) => {
 
         const result = await nipFlow.searchInvalidNip(dataset.nip);
 
