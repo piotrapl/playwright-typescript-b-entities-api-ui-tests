c8e89f522abaa42304e1e953228f4fe9a0dc5a4d
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-17 12:38:59 +0200

    definicja metody open()

--- pages/nip.page.ts
+++ pages/nip.page.ts
@@ -2,8 +2,19 @@ import { Page } from '@playwright/test';
 import { ENV } from '../utils/env';
 import { ApiResponse } from '../types/api.types';
 
-    
+
 // NipPage - definicja klasy NipPage,
 //           będzie zawierać metody do interakcji z elementami strony związanymi z NIP-ami.
 export class NipPage {
+
     constructor (private page: Page) {}
+
+    async open() {
+
+        await this.page.goto(ENV.baseURL);
+
+// getByLabel - metoda lokalizująca element na stronie na podstawie jego etykiety (label).
+// waitFor - metoda - czeka, aż element będzie dostępny na stronie, zanim przejdzie się do kolejnych kroków testu.
+        await this.page.getByLabel('NIP', { exact: true }).waitFor();
+    }
+}
