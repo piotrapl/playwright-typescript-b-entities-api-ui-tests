510b104e4022e0c364a1352c873bbd61b973ff12
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-17 12:27:58 +0200

    dodanie kolejnego importu, konstruktora klasy nipPage

--- pages/nip.page.ts
+++ pages/nip.page.ts
@@ -1,6 +1,8 @@
 import { Page } from '@playwright/test';
 import { ENV } from '../utils/env';
+import { ApiResponse } from '../types/api.types';
 
+    
 // NipPage - definicja klasy NipPage,
 //           będzie zawierać metody do interakcji z elementami strony związanymi z NIP-ami.
 export class NipPage {
