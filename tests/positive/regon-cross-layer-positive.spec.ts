4cccfa40071227765e34eeab0fb46fb76f99f35e
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-19 15:28:57 +0200

    nowy plik z definicjami testów; plik z danymi

---
+++ tests/positive/regon-cross-layer-positive.spec.ts
@@ -0,0 +1,11 @@
+import { test } from '../../fixtures/test-fixtures';
+import { validRegons } from '../../data/positive/valid-regons';
+
+for (const dataset of validRegons) {
+  // definicja testu dla każdego zestawu danych z istniejącymi REGONami
+  // test() - to funkcja z frameworka testowego
+  test(`REGON positive search: ${dataset.regon}`, async ({ regonPositiveFlow }) => {
+    const result = await regonPositiveFlow.searchExistingRegonAndVerify(dataset.regon);
+    await result.assert();
+  });
+}
