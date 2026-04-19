12c38560a668881e325e85b7fa125a340c214c34
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-19 12:22:10 +0200

    + definicje 2 kolejnych kroków + fraza return

--- flows/regon.positive.flow.ts
+++ flows/regon.positive.flow.ts
@@ -9,6 +9,15 @@ export class RegonPositiveFlow {
     await test.step('Open REGON search page', async () => {
       await this.regonPage.open();
     });
+
+    let apiResponse;
+    await test.step('Search REGON and capture backend response', async () => {
+      apiResponse = await this.regonPage.searchRegon(regon);
+    });
+
+    return new RegonPositiveAssertions(
+      this.regonPage,
+      apiResponse!
+    );
   }
- 
 }
