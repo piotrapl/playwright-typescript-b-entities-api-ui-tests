f9364201eb2134f9312a9adeb00444451757f4b7
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-19 12:20:09 +0200

    dodanie definicji nowej klasy flow i def. kroku nr 1

--- flows/regon.positive.flow.ts
+++ flows/regon.positive.flow.ts
@@ -1,3 +1,14 @@
 import { test } from '@playwright/test';
 import { RegonPage } from '../pages/regon.page';
 import { RegonPositiveAssertions } from '../assertions/regon.positive.assert';
+
+export class RegonPositiveFlow {
+  constructor(private regonPage: RegonPage) {}
+
+  async searchExistingRegonAndVerify(regon: string) {
+    await test.step('Open REGON search page', async () => {
+      await this.regonPage.open();
+    });
+  }
+ 
+}
