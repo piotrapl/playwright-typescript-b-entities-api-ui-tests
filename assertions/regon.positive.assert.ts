8fd5fdc157b29459e83465ff2b40542f719abed6
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-19 12:26:01 +0200

    + definicja nowej klasy, jej konstruktora

--- assertions/regon.positive.assert.ts
+++ assertions/regon.positive.assert.ts
@@ -1,3 +1,11 @@
 import { expect } from '@playwright/test';
 import { ApiResponse } from '../types/api.types';
 import { RegonPage } from '../pages/regon.page';
+
+export class RegonPositiveAssertions {
+  constructor(
+    private regonPage: RegonPage,
+    private apiResponse: ApiResponse
+  ) {}
+
+}
