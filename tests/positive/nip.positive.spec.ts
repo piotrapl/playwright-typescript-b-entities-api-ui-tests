b6ef56a31bbd034cbfb5a626ab231b54e3e86e69
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-20 16:29:02 +0200

    poprawa ścieżki w poleceniu import

--- tests/positive/nip.positive.spec.ts
+++ tests/positive/nip.positive.spec.ts
@@ -1,5 +1,5 @@
 import { test } from '../../fixtures/test-fixtures';
-import { validNips } from '../../data/positive/valid.nips';
+import { validNips } from '../../data/valid-nips';
 
 for (const dataset of validNips) {
   test(`NIP positive search: ${dataset.nip}`, async ({ nipPositiveFlow }) => {
