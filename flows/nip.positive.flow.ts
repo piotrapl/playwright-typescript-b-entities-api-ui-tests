bd9f88ecf1d6a9d6770c70400b9584fb53c76c92
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-22 17:22:36 +0200

    poprawa błędu - dostosowanie do nowej klasy typu Page

--- flows/nip.positive.flow.ts
+++ flows/nip.positive.flow.ts
@@ -1,22 +1,22 @@
 import { test } from '@playwright/test';
-import { NipPage } from '../pages/nip.page';
+import { SearchPage } from '../pages/search.page';
 import { NipPositiveAssertions } from '../assertions/nip.positive.assert';
 
 export class NipPositiveFlow {
-  constructor(private nipPage: NipPage) {}
+  constructor(private searchPage: SearchPage) {}
 
   async searchExistingNipAndVerify(nip: string) {
     await test.step('Open NIP search page', async () => {
-      await this.nipPage.open();
+      await this.searchPage.open();
     });
 
     let apiResponse;
     await test.step('Search NIP and capture backend response', async () => {
-      apiResponse = await this.nipPage.searchByNip(nip);
+      apiResponse = await this.searchPage.searchByNip(nip);
     });
 
     return new NipPositiveAssertions(
-      uiMessage!,
+      this.searchPage,
       apiResponse!
     );
   }
