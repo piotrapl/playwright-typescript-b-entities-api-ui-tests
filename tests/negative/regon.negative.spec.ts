ae9c1b36c924664b5b4fc5887e478585f6c6a4b0
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-22 19:24:49 +0200

    Update regon.negative.spec.ts

--- tests/negative/regon.negative.spec.ts
+++ tests/negative/regon.negative.spec.ts
@@ -5,7 +5,7 @@ import { invalidLongRegons } from '../../data/invalid-regons';
 for (const dataset of invalidRegons) {
 
     
-  test(`REGON negative search: ${dataset.regon}`, async ({ regonFlow }) => {
+  test(`Próba wyszukiwania po nieistniejącym REGON-ie 9-cyfrowym: ${dataset.regon}`, async ({ regonFlow }) => {
 
         const result = await regonFlow.searchRegonAndVerify(dataset.regon);
 
@@ -18,7 +18,7 @@ for (const dataset of invalidRegons) {
 for (const dataset of invalidLongRegons) {
 
     
-  test(`REGON negative search: ${dataset.regon}`, async ({ regonFlow }) => {
+  test(`Próba wyszukiwania po nieistniejącym REGON-ie 14-cyfrowym: ${dataset.regon}`, async ({ regonFlow }) => {
 
         const result = await regonFlow.searchRegonAndVerify(dataset.regon);
 
