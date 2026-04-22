c16737a9ccc0a15e23af74d90a0b87b086e39284
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-22 20:02:31 +0200

    uzupełnienie danych testowych, zmiana opisów

--- data/valid-nips.ts
+++ data/valid-nips.ts
@@ -1,5 +1,9 @@
 export const validNips = [
   {
     nip: '6770072027' // istnieje podmiot o takim nipie
+  },
+
+  {
+    nip: '5260026186' // istnieje podmiot o takim nipie
   }
 ];
--- tests/positive/nip.positive.spec.ts
+++ tests/positive/nip.positive.spec.ts
@@ -2,7 +2,7 @@ import { test } from '../../fixtures/test-fixtures';
 import { validNips } from '../../data/valid-nips';
 
 for (const dataset of validNips) {
-  test(`NIP positive search: ${dataset.nip}`, async ({ nipPositiveFlow }) => {
+  test(`Wyszukiwanie po istniejącym NIP-ie: ${dataset.nip}`, async ({ nipPositiveFlow }) => {
     const result = await nipPositiveFlow.searchExistingNipAndVerify(dataset.nip);
     await result.assert();
   });
