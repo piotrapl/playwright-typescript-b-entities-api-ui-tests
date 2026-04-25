b3017f790608e969c2ef47b6ef0fd91498038fc2
Piotr Amrozy <piotr.a.pl@outlook.com>
2026-04-25 16:50:14 +0200

    nowe pliki z danymi (numery KRS)

---
+++ data/valid-krsnumbers.ts
@@ -0,0 +1,7 @@
+export const validKrsNumbers = [
+
+    {
+        krs: '0000660949' // istnieje podmiot o takim krs-ie
+    }
+ 
+]   
--- data/valid-nips.ts
+++ data/valid-nips.ts
@@ -1,9 +1,5 @@
 export const validNips = [
   {
-    nip: '6770072027' // istnieje podmiot o takim nipie
-  },
-
-  {
-    nip: '5260026186' // istnieje podmiot o takim nipie
+    nip: '0000660949' // istnieje podmiot o takim krs    
   }
 ];
